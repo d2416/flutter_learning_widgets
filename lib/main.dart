eacd49a454f38d639d2a57dbf428bcf46603d2c3
David <davidpineda.co@gmail.com>
2021-01-24 18:59:12 +0000

    add contact details - Manipulating Rows, Columns, Icons, Fonts, styles

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,54 @@ class MiCardApp extends StatelessWidget {
                   fontFamily: 'Source Sans Pro',
                 ),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 25.0, horizontal: 25.0),
+                padding: EdgeInsets.all(15.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+385 12 345 6789',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'Source Sans Pro',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 50.0),
+                padding: EdgeInsets.all(15.0),
+                color: Colors.white,
+                child: Row(children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 20.0,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'davidpineda@email.com',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ]),
+              ),
             ],
           ),
         ),
