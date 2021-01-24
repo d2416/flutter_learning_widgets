3472e0b5689ed3c1410b245830b00fcd7b0bfa52
David <davidpineda.co@gmail.com>
2021-01-24 13:31:32 +0000

    use crossAxisAlignment: CrossAxisAlignment.stretch as Column property instead of double.infinity in each Container

--- lib/main.dart
+++ lib/main.dart
@@ -15,31 +15,25 @@ class MiCardApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             verticalDirection: VerticalDirection.up,
             mainAxisSize: MainAxisSize.max,
             children: [
               Container(
                 color: Colors.blue,
-                width: double.infinity,
                 height: 100.0,
                 child: Text("container 1"),
               ),
               Container(
                 color: Colors.white,
                 child: Text("container 2"),
-                width: double.infinity,
                 height: 100.0,
               ),
               Container(
                 color: Colors.red,
-                width: double.infinity,
                 height: 100.0,
                 child: Text("container 3"),
               ),
-              Container(
-                width: double.infinity,
-              )
             ],
           ),
         ),
