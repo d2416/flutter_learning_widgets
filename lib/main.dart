5b5e44dcce0aa998b94ea5ae347b96d79e049d2b
David <davidpineda.co@gmail.com>
2021-01-24 12:08:38 +0000

    using a single-child layout widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,16 @@ class MiCardApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.blue,
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.all(25),
+            padding: EdgeInsets.fromLTRB(25, 15, 10, 30),
+            child: Text("hello"),
+          ),
+        ),
       ),
     );
   }
